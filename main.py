558ac0bde72226de7cc0d98f6935b3f8dc1105cb
Tomas-Mecius <tmecius@gmail.com>
2020-12-28 08:24:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+# import colorgram
+# rgb_colors = []
+# colors = colorgram.extract("image.jpg", 22)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+
+color_list = [(208, 159, 108), (223, 206, 117),
+               (136, 173, 194), (242, 250, 245), (252, 245, 249),
+               (217, 232, 241), (38, 106, 140), (137, 184, 146),
+               (13, 52, 76), (220, 87, 63), (146, 80, 71), (69, 165, 119),
+               (29, 129, 106), (125, 80, 96), (9, 59, 50), (54, 153, 180),
+               (197, 130, 144), (52, 33, 43), (129, 36, 49), (207, 83, 101),
+               (4, 111, 88)]
+
